fc19eaa0f0f7e8902033ad67a6905fa69080225e
zemola <azeemolatunji@gmail.com>
2022-06-11 11:44:56 +0100

    Navigation component renderd

---
+++ src/component/navigation/Nav.js
@@ -0,0 +1,21 @@
+import './Nav.css';
+import React from 'react';
+
+function Nav() {
+  return (
+    <div className='container'>
+      <h1 className='logo'>Logo</h1>
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          About
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default Nav
